ac189f5c32419d08f211a29231c8c413fe27da25
Apple <apple@Apples-MacBook-Pro.local>
2018-10-20 22:03:58 +0530

    Get File Working

--- ToyUp_rest/fileHandler/views.py
+++ ToyUp_rest/fileHandler/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
+from .models import File
 
 from .serializers import FileSerializer
 
@@ -10,6 +11,12 @@ from .serializers import FileSerializer
 class FileView(APIView):
 
   parser_classes = (MultiPartParser, FormParser)
+
+  def get(self, request):
+
+        files = File.objects.all()
+        files_serialized = FileSerializer(files, many=True).data
+        return Response(files_serialized)
 
   def post(self, request, *args, **kwargs):
 
